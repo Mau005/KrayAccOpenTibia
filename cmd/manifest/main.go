3e8fca0248bb826a6867010092cbcd44de48e6fd
mau <mpino1701@gmail.com>
2025-08-17 01:58:12 -0400

    add manifest secuence in server

--- cmd/manifest/main.go
+++ cmd/manifest/main.go
@@ -2,35 +2,36 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/Mau005/KrayAccOpenTibia/src/config"
 	"github.com/Mau005/KrayAccOpenTibia/src/controller"
+	"github.com/Mau005/KrayAccOpenTibia/src/handler"
+	"github.com/Mau005/KrayAccOpenTibia/src/middleware"
 	"github.com/Mau005/KrayAccOpenTibia/src/models"
+	"github.com/Mau005/KrayAccOpenTibia/src/utils"
+	"github.com/gorilla/mux"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	// Carpeta local donde tienes los archivos del cliente
-	err := config.Load("config.yml")
-	if err != nil {
-		log.Println(err)
-		return
-	}
+func create_manifest() {
+
 	root := "./Client"
 
-	// Manifest inicial
 	var m models.Manifest
 	m.App = "AinhoOT"
 	m.Version = os.Getenv("VERSION")
-	m.BaseURL = fmt.Sprintf("http://%s:%d/launcher_client", config.Global.ServerWeb.IP, config.Global.ServerWeb.Port) // Nueva ruta base
-
+	if m.Version == "" {
+		m.Version = "1.0.0"
+	}
+	m.BaseURL = fmt.Sprintf("http://%s:%d/launcher_client", config.Global.ServerWeb.IP, config.Global.ServerWeb.Port)
 	var lauch controller.LaucherController
 
-	// Recorremos todos los archivos de la carpeta root
-	err = filepath.WalkDir(root, func(p string, d os.DirEntry, err error) error {
+	err := filepath.WalkDir(root, func(p string, d os.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -43,7 +44,6 @@ func main() {
 			return fmt.Errorf("error al hashear %s: %w", p, err)
 		}
 
-		// Obtener ruta relativa para guardar en el manifest
 		rel, _ := filepath.Rel(root, p)
 		m.Files = append(m.Files, models.FileEntry{
 			Path:   filepath.ToSlash(rel),
@@ -54,16 +54,89 @@ func main() {
 		return nil
 	})
 	if err != nil {
-		fmt.Println("Error al recorrer archivos:", err)
-		os.Exit(1)
+		utils.ErrorFatal("Error while browsing files", err.Error())
+		return
 	}
 
 	// Exportar manifest.json
 	out, _ := json.MarshalIndent(m, "", "  ")
 	if err := os.WriteFile("manifest.json", out, 0644); err != nil {
-		fmt.Println("Error al guardar manifest.json:", err)
-		os.Exit(1)
+		utils.ErrorFatal("error save manifest.json:", err.Error())
+		return
 	}
 
-	fmt.Println("Manifest generado correctamente en manifest.json")
+	utils.InfoBlue("Manifest generated successfully in manifest.json")
+}
+
+func loadDependency() error {
+	global := &models.Configuration{}
+	content, err := os.ReadFile("config.yml")
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(content, &global)
+	if err != nil {
+		return err
+	}
+	config.Global = global
+
+	man, err := os.ReadFile("manifest.json")
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(man, &config.Manifest)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	err := loadDependency()
+	if err != nil {
+		utils.ErrorFatal(err.Error())
+	}
+
+	preparing := flag.Bool("create_manifest", false, "Create manifest to client ")
+	flag.Parse()
+	if *preparing {
+		create_manifest()
+		return
+	}
+	controller.InitTemporaryEmpty()
+
+	r := mux.NewRouter()
+	lauch := http.FileServer(http.Dir("./client"))
+	r.PathPrefix("/launcher_client").Handler(http.StripPrefix("/launcher_client", lauch))
+
+	r.HandleFunc("/get_news_short", func(w http.ResponseWriter, r *http.Request) {
+		var listNews []models.NewsShort
+		listNews = append(listNews, models.NewsShort{
+			IconID:      1,
+			Description: "Test News Example",
+		})
+
+		json.NewEncoder(w).Encode(struct {
+			NewsShort []models.NewsShort
+		}{
+			NewsShort: listNews,
+		})
+	}).Methods("GET")
+
+	ctl := r.PathPrefix("/client").Subrouter()
+	ctl.Use(middleware.CommonMiddleware)
+
+	var maniHandler handler.ManifestHandler
+	ctl.HandleFunc("/manifest", maniHandler.GetManifiest).Methods("GET")
+	ctl.HandleFunc("/info", maniHandler.GetInfo).Methods("GET")
+
+	ip := fmt.Sprintf("%s:%d", config.Global.ServerWeb.IP, config.Global.ServerWeb.Port)
+
+	utils.InfoBlue(fmt.Sprintf("[HTTP] Starting the HTTP server: http://%s/", ip))
+	if err := http.ListenAndServe(ip, r); err != nil {
+		utils.ErrorFatal("Error starting HTTP server: " + err.Error())
+	}
+
 }
